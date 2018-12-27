988a45a51782a658ec0a106f0a3a71cd92b152b2
Brisco Laña <bat1brisco@gmail.com>
2018-12-27 15:44:39 +0800

    uncommited works

--- rcis/api/routes/routes.py
+++ rcis/api/routes/routes.py
@@ -1,8 +1,8 @@
 from flask import jsonify, Blueprint
-from controller.usercontroller import *
+from module.controller.usercontroller import *
 user = Blueprint('user', __name__)
 
 @user.route('/test/<bran>', methods=['GET'])
 def home(bran):
-    d = 
+    
     return jsonify({'dong': bran})
