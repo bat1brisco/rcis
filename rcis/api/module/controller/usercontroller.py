828f60bf0768555992ae66acdce42e3a7e81c65b
Brisco Laña <bat1brisco@gmail.com>
2018-12-30 01:47:11 +0800

    unsaved

---
+++ rcis/api/module/controller/usercontroller.py
@@ -0,0 +1,10 @@
+import requests
+from flask import Flask, request, jsonify
+
+
+class usr_controller():
+    def __init__(self, arg):
+        self.arg = arg
+
+    def main():
+        return {'dong': bran}
--- rcis/api/routes/routes.py
+++ rcis/api/routes/routes.py
@@ -4,5 +4,5 @@ user = Blueprint('user', __name__)
 
 @user.route('/test/<bran>', methods=['GET'])
 def home(bran):
-    
+    res = usr_controller.main()
     return jsonify({'dong': bran})
